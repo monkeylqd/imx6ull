a421938ca701324fcabf2af634a6c28d2e0ad0e7
monkeylqd <864535720@qq.com>
2025-07-11 15:12:53 +0800

    UI布局完成

--- app/qt/QT_serial/mainwindow.cpp
+++ app/qt/QT_serial/mainwindow.cpp
@@ -12,24 +12,12 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-#if USE_DEBUG
-    m_serial = new QSerialPort();
-    m_serial->setPortName(SERIAL_1_NAME);
-    m_serial->open(QIODevice::ReadWrite);
-    m_serial->setBaudRate(QSerialPort::Baud115200);
-    m_serial->setDataBits(QSerialPort::Data8);
-    m_serial->setParity(QSerialPort::NoParity);
-    m_serial->setStopBits(QSerialPort::OneStop);
-    m_serial->setFlowControl(QSerialPort::NoFlowControl);
 
-    connect(m_serial, SIGNAL(readyRead()), this, SLOT(read_serial_data()));
-#else
     m_comm_rev_flag = 0; //初始化flag标志
     m_serial_vol_curr_CH01 = new QSerialPort();
     m_serial_vol_curr_CH01->setPortName(SERIAL_VOL_CURR_CH01_NAME);
     m_serial_vol_curr_CH01->open(QIODevice::ReadWrite);
     m_serial_vol_curr_CH01->setBaudRate(QSerialPort::Baud9600);
-//    m_serial_vol_curr_CH01->setBaudRate(QSerialPort::Baud9600);
     m_serial_vol_curr_CH01->setDataBits(QSerialPort::Data8);
     m_serial_vol_curr_CH01->setParity(QSerialPort::NoParity);
     m_serial_vol_curr_CH01->setStopBits(QSerialPort::OneStop);
@@ -66,11 +54,22 @@ MainWindow::MainWindow(QWidget *parent)
     connect(m_serial_vol_curr_CH02, SIGNAL(readyRead()), this, SLOT(read_serial_vol_curr_CH02()));
     connect(m_serial_vol_curr_CH03, SIGNAL(readyRead()), this, SLOT(read_serial_vol_curr_CH03()));
     connect(m_serial_power_communication, SIGNAL(readyRead()), this, SLOT(read_serial_power_communication()));
-#endif
+
 
     m_timer = new QTimer(this);
     connect(m_timer, SIGNAL(timeout()), this, SLOT(on_timeout()));
-    m_timer->start(2000);
+    m_timer->start(1000);
+
+    m_master_timer = new QTimer(this);
+    connect(m_master_timer, SIGNAL(timeout()), this, SLOT(on_master_timeout()));
+
+    m_slave_timer = new QTimer(this);
+    connect(m_slave_timer, SIGNAL(timeout()), this, SLOT(on_slave_timeout()));
+    m_slave_timer->start(3000);
+
+    // 发送查找ID的命令
+//    m_serial_power_communication->writeData((const char *)("AT+ADDR?\r\n"), 10);
+    m_serial_power_communication->write("AT+ADDR?\r\n");
 }
 
 MainWindow::~MainWindow()
@@ -78,7 +77,7 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::parseMeasurementData(int index_salve, QString inputs)
+void MainWindow::parse_vol_cur_data(int index_ch, QString inputs)
 {
     QString test1 = inputs.remove(' ');
     QString input = test1.split("\r\n").first();
@@ -87,6 +86,112 @@ void MainWindow::parseMeasurementData(int index_salve, QString inputs)
     for(int i = 0; i < parts.size(); i++)
     {
         qDebug()<<parts[i];
+    }
+
+    if((m_id == 1) && (index_ch == 0))
+    {
+        QStringList vol_list = parts[0].split(":", QString::SkipEmptyParts);
+        ui->salve_01_ch01_vol->setText(vol_list[1]);
+        QStringList cur_list = parts[1].split(":", QString::SkipEmptyParts);
+        ui->salve_01_ch01_cur->setText(cur_list[1]);
+    }
+    else if((m_id == 1) && (index_ch == 1))
+    {
+        QStringList vol_list = parts[0].split(":", QString::SkipEmptyParts);
+        ui->salve_01_ch02_vol->setText(vol_list[1]);
+        QStringList cur_list = parts[1].split(":", QString::SkipEmptyParts);
+        ui->salve_01_ch02_cur->setText(cur_list[1]);
+    }
+    else if((m_id == 1) && (index_ch == 2))
+    {
+        QStringList vol_list = parts[0].split(":", QString::SkipEmptyParts);
+        ui->salve_01_ch03_vol->setText(vol_list[1]);
+        QStringList cur_list = parts[1].split(":", QString::SkipEmptyParts);
+        ui->salve_01_ch03_cur->setText(cur_list[1]);
+    }
+    else if((m_id == 2) && (index_ch == 0))
+    {
+        QStringList vol_list = parts[0].split(":", QString::SkipEmptyParts);
+        ui->salve_02_ch01_vol->setText(vol_list[1]);
+        QStringList cur_list = parts[1].split(":", QString::SkipEmptyParts);
+        ui->salve_02_ch01_cur->setText(cur_list[1]);
+    }
+    else if((m_id == 2) && (index_ch == 1))
+    {
+        QStringList vol_list = parts[0].split(":", QString::SkipEmptyParts);
+        ui->salve_02_ch02_vol->setText(vol_list[1]);
+        QStringList cur_list = parts[1].split(":", QString::SkipEmptyParts);
+        ui->salve_02_ch02_cur->setText(cur_list[1]);
+    }
+    else if((m_id == 2) && (index_ch == 2))
+    {
+        QStringList vol_list = parts[0].split(":", QString::SkipEmptyParts);
+        ui->salve_02_ch03_vol->setText(vol_list[1]);
+        QStringList cur_list = parts[1].split(":", QString::SkipEmptyParts);
+        ui->salve_02_ch03_cur->setText(cur_list[1]);
+    }
+    else if((m_id == 3) && (index_ch == 0))
+    {
+        QStringList vol_list = parts[0].split(":", QString::SkipEmptyParts);
+        ui->salve_03_ch01_vol->setText(vol_list[1]);
+        QStringList cur_list = parts[1].split(":", QString::SkipEmptyParts);
+        ui->salve_03_ch01_cur->setText(cur_list[1]);
+    }
+    else if((m_id == 3) && (index_ch == 1))
+    {
+        QStringList vol_list = parts[0].split(":", QString::SkipEmptyParts);
+        ui->salve_03_ch02_vol->setText(vol_list[1]);
+        QStringList cur_list = parts[1].split(":", QString::SkipEmptyParts);
+        ui->salve_03_ch02_cur->setText(cur_list[1]);
+    }
+    else if((m_id == 3) && (index_ch == 2))
+    {
+        QStringList vol_list = parts[0].split(":", QString::SkipEmptyParts);
+        ui->salve_03_ch03_vol->setText(vol_list[1]);
+        QStringList cur_list = parts[1].split(":", QString::SkipEmptyParts);
+        ui->salve_03_ch03_cur->setText(cur_list[1]);
+    }
+    else if((m_id == 4) && (index_ch == 0))
+    {
+        QStringList vol_list = parts[0].split(":", QString::SkipEmptyParts);
+        ui->salve_04_ch01_vol->setText(vol_list[1]);
+        QStringList cur_list = parts[1].split(":", QString::SkipEmptyParts);
+        ui->salve_04_ch01_cur->setText(cur_list[1]);
+    }
+    else if((m_id == 4) && (index_ch == 1))
+    {
+        QStringList vol_list = parts[0].split(":", QString::SkipEmptyParts);
+        ui->salve_04_ch02_vol->setText(vol_list[1]);
+        QStringList cur_list = parts[1].split(":", QString::SkipEmptyParts);
+        ui->salve_04_ch02_cur->setText(cur_list[1]);
+    }
+    else if((m_id == 4) && (index_ch == 2))
+    {
+        QStringList vol_list = parts[0].split(":", QString::SkipEmptyParts);
+        ui->salve_04_ch03_vol->setText(vol_list[1]);
+        QStringList cur_list = parts[1].split(":", QString::SkipEmptyParts);
+        ui->salve_04_ch03_cur->setText(cur_list[1]);
+    }
+    else if((m_id == 5) && (index_ch == 0))
+    {
+        QStringList vol_list = parts[0].split(":", QString::SkipEmptyParts);
+        ui->salve_05_ch01_vol->setText(vol_list[1]);
+        QStringList cur_list = parts[1].split(":", QString::SkipEmptyParts);
+        ui->salve_05_ch01_cur->setText(cur_list[1]);
+    }
+    else if((m_id == 5) && (index_ch == 1))
+    {
+        QStringList vol_list = parts[0].split(":", QString::SkipEmptyParts);
+        ui->salve_05_ch02_vol->setText(vol_list[1]);
+        QStringList cur_list = parts[1].split(":", QString::SkipEmptyParts);
+        ui->salve_05_ch02_cur->setText(cur_list[1]);
+    }
+    else if((m_id == 5) && (index_ch == 2))
+    {
+        QStringList vol_list = parts[0].split(":", QString::SkipEmptyParts);
+        ui->salve_05_ch03_vol->setText(vol_list[1]);
+        QStringList cur_list = parts[1].split(":", QString::SkipEmptyParts);
+        ui->salve_05_ch03_cur->setText(cur_list[1]);
     }
 
     // 确保有6个部分
@@ -152,33 +257,155 @@ void MainWindow::parseMeasurementData(int index_salve, QString inputs)
     // 打印解析结果
     for(int i = 0; i < measurements.size(); i++)
     {
-        m_CH_value[index_salve][i] = measurements.at(i).value;
+        m_CH_value[index_ch][i] = measurements.at(i).value;
     }
-    qDebug()<<m_CH_value[index_salve][0]<<" "<<m_CH_value[index_salve][1]<<" "<<m_CH_value[index_salve][2]<<" "<<m_CH_value[index_salve][3]<<" "<<m_CH_value[index_salve][4]<<" "<<m_CH_value[index_salve][5];
+    qDebug()<<m_CH_value[index_ch][0]<<" "<<m_CH_value[index_ch][1]<<" "<<m_CH_value[index_ch][2]<<" "<<m_CH_value[index_ch][3]<<" "<<m_CH_value[index_ch][4]<<" "<<m_CH_value[index_ch][5];
 #endif
     for (const auto &m : measurements) {
         qDebug() << m.name << ":" << m.value << m.unit;
     }
 
 }
-
-#if USE_DEBUG
-void MainWindow::read_serial_data()
+// COMM接收的数据类型有：
+// +ADDR=000000000001
+// +RX=00,000000000001,0009,123456789
+// +RX=01,000000000001,0009,123456789
+// +TX=OK
+int MainWindow::parse_comm_data(QString str)
 {
-    QByteArray read_buff;
-    read_buff = m_serial->readAll();
-//    ui->textBrowser->setText(read_buff.toStdString());
-    ui->textBrowser->setText(QString(read_buff));
-    qDebug("read:%s\n", qPrintable(QString(read_buff)));
+    bool ok;
+    qint64 rev_id = 0;
+    qint64 rev_len = 0;
+    float vol_cur[6];
+    if(str.contains("+ADDR="))
+    {
+        QStringList parts = str.split("=");
+        if(parts.size() >= 2)
+        {
+            m_ID = parts[1];
+            m_id = parts[1].toLongLong(&ok, 16);
+            qDebug()<<"m_ID:"<<m_ID;
+        }
+    }
+    else if(str.contains("+RX="))
+    {
+        QStringList parts = str.split(",");
+        if(parts.size() != 4)
+        {
+            qDebug()<<"error:"<<str;
+            return -1;
+        }
+
+        QStringList temp = parts[0].split("=");
+        if(temp[1].contains("00"))
+        {
+            // 00-点对点通信，需要回复    01-广播通信，不需要回复
+        }
+
+        // 解析设备ID，通过ID确认设备是否已经记录
+        if(! m_dev_list.contains(parts[1]))
+        {
+            m_dev_list.append(parts[1]);
+        }
+        rev_id = parts[1].toLongLong(&ok, 16);
+        if(!ok)
+        {
+            qDebug()<<"change ID to Hex fail.";
+        }
+
+        // 解析设备发送的数据长度
+        rev_len = parts[2].toLongLong(&ok, 16);
+        if(!ok)
+        {
+            qDebug()<<"change Len to Hex fail.";
+        }
+
+        // 解析设备的数据
+        QByteArray rev_data = parts[3].toUtf8();
+        const char* charPtr = rev_data.constData();
+        if(check_data(charPtr, rev_len) != 0)
+        {
+            qDebug()<<"rev data check fail.";
+            return -1;
+        }
+
+        // 收到的数据是常规数据
+        if(DATA_IS_DATA == charPtr[1])
+        {
+            memcpy(&vol_cur, &charPtr[2], 24);
+            memcpy(&m_ctl_value, &charPtr[26], 5);
+            if(rev_id == 1)
+            {
+                ui->salve_01_ch01_vol->setText(QLocale().toString(vol_cur[0]) + "V");
+                ui->salve_01_ch02_vol->setText(QLocale().toString(vol_cur[2]) + "V");
+                ui->salve_01_ch03_vol->setText(QLocale().toString(vol_cur[4]) + "V");
+                ui->salve_01_ch01_cur->setText(QLocale().toString(vol_cur[1]) + "A");
+                ui->salve_01_ch02_cur->setText(QLocale().toString(vol_cur[3]) + "A");
+                ui->salve_01_ch03_cur->setText(QLocale().toString(vol_cur[5]) + "A");
+            }
+            else if(rev_id == 2)
+            {
+                ui->salve_02_ch01_vol->setText(QLocale().toString(vol_cur[0]) + "V");
+                ui->salve_02_ch02_vol->setText(QLocale().toString(vol_cur[2]) + "V");
+                ui->salve_02_ch03_vol->setText(QLocale().toString(vol_cur[4]) + "V");
+                ui->salve_02_ch01_cur->setText(QLocale().toString(vol_cur[1]) + "A");
+                ui->salve_02_ch02_cur->setText(QLocale().toString(vol_cur[3]) + "A");
+                ui->salve_02_ch03_cur->setText(QLocale().toString(vol_cur[5]) + "A");
+            }
+            else if(rev_id == 3)
+            {
+                ui->salve_03_ch01_vol->setText(QLocale().toString(vol_cur[0]) + "V");
+                ui->salve_03_ch02_vol->setText(QLocale().toString(vol_cur[2]) + "V");
+                ui->salve_03_ch03_vol->setText(QLocale().toString(vol_cur[4]) + "V");
+                ui->salve_03_ch01_cur->setText(QLocale().toString(vol_cur[1]) + "A");
+                ui->salve_03_ch02_cur->setText(QLocale().toString(vol_cur[3]) + "A");
+                ui->salve_03_ch03_cur->setText(QLocale().toString(vol_cur[5]) + "A");
+            }
+            else if(rev_id == 4)
+            {
+                ui->salve_04_ch01_vol->setText(QLocale().toString(vol_cur[0]) + "V");
+                ui->salve_04_ch02_vol->setText(QLocale().toString(vol_cur[2]) + "V");
+                ui->salve_04_ch03_vol->setText(QLocale().toString(vol_cur[4]) + "V");
+                ui->salve_04_ch01_cur->setText(QLocale().toString(vol_cur[1]) + "A");
+                ui->salve_04_ch02_cur->setText(QLocale().toString(vol_cur[3]) + "A");
+                ui->salve_04_ch03_cur->setText(QLocale().toString(vol_cur[5]) + "A");
+            }
+            else if(rev_id == 5)
+            {
+                ui->salve_05_ch01_vol->setText(QLocale().toString(vol_cur[0]) + "V");
+                ui->salve_05_ch02_vol->setText(QLocale().toString(vol_cur[2]) + "V");
+                ui->salve_05_ch03_vol->setText(QLocale().toString(vol_cur[4]) + "V");
+                ui->salve_05_ch01_cur->setText(QLocale().toString(vol_cur[1]) + "A");
+                ui->salve_05_ch02_cur->setText(QLocale().toString(vol_cur[3]) + "A");
+                ui->salve_05_ch03_cur->setText(QLocale().toString(vol_cur[5]) + "A");
+            }
+        }
+        else if(DATA_IS_MASTER == charPtr[1])
+        {
+            // 收到竞选master的消息
+            qDebug()<<rev_id<<" campaign";
+        }
+
+    }
+
+    return 0;
 }
 
-
-void MainWindow::on_pushButton_clicked()
+int MainWindow::check_data(const char *data, int len)
 {
-    QByteArray send_data = ui->textEdit->toPlainText().toUtf8();
-    m_serial->write(send_data);
+    char value = 0;
+    for(int i = 0; i < len-2; i++)
+    {
+        value += data[i];
+    }
+
+    if(value == data[len-1])
+    {
+        return 0;
+    }
+    return -1;
 }
-#else
+
 
 void MainWindow::read_serial_vol_curr_CH01()
 {
@@ -186,7 +413,7 @@ void MainWindow::read_serial_vol_curr_CH01()
     cust_delay(1000);
     read_buff = m_serial_vol_curr_CH01->readAll();
     qDebug("CH01 read:%s\n", qPrintable(QString(read_buff)));
-    parseMeasurementData(0, QString(read_buff));
+    parse_vol_cur_data(0, QString(read_buff));
 
 }
 
@@ -196,7 +423,7 @@ void MainWindow::read_serial_vol_curr_CH02()
     cust_delay(1000);
     read_buff = m_serial_vol_curr_CH02->readAll();
     qDebug("CH02 read:%s\n", qPrintable(QString(read_buff)));
-    parseMeasurementData(1, QString(read_buff));
+    parse_vol_cur_data(1, QString(read_buff));
 }
 
 void MainWindow::read_serial_vol_curr_CH03()
@@ -205,34 +432,189 @@ void MainWindow::read_serial_vol_curr_CH03()
     cust_delay(1000);
     read_buff = m_serial_vol_curr_CH03->readAll();
     qDebug("CH03 read:%s\n", qPrintable(QString(read_buff)));
-    parseMeasurementData(2, QString(read_buff));
+    parse_vol_cur_data(2, QString(read_buff));
 }
 
 void MainWindow::read_serial_power_communication()
 {
     QByteArray read_buff;
+    cust_delay(120);
+    m_comm_rev_flag = 1;        // 电力载波通信有收到数据
     read_buff = m_serial_power_communication->readAll();
     qDebug("COMM read:%s\n", qPrintable(QString(read_buff)));
 }
-#endif
 
 void MainWindow::on_timeout()
 {
     qDebug()<<"this is timeout";
-#if USE_DEBUG
-#else
-    m_timer->stop();
-    if(m_comm_rev_flag == 0)
-    {
 
-    }
-//    m_serial_vol_curr_CH01->write(" | Vrms: 221.0V | Irms: 0.1A | P: 0.0000W | PF: 0.00000 | F: 0.0000Hz | W: 0.0035KW*H |");
-//    m_serial_vol_curr_CH02->write(" | Vrms: 222.12V | Irms: 0.2A | P: 0.0000W | PF: 0.00000 | F: 0.0000Hz | W: 0.0035KW*H |");
-//    m_serial_vol_curr_CH03->write(" | Vrms: 223.1V | Irms: 0.3A | P: 0.0000W | PF: 0.00000 | F: 0.0000Hz | W: 0.0035KW*H |");
+    m_timer->stop();
     m_serial_vol_curr_CH01->write(">>GetVal");
     m_serial_vol_curr_CH02->write(">>GetVal");
     m_serial_vol_curr_CH03->write(">>GetVal");
-    m_serial_power_communication->write("I am is COMM");
     m_timer->start(2000);
-#endif
+
+}
+
+void MainWindow::on_master_timeout()
+{
+
+}
+
+void MainWindow::on_slave_timeout()
+{
+    if(m_comm_rev_flag == 0)
+    {
+        m_master_timer->start(1000);
+    }
+}
+
+void MainWindow::on_salve_01_ctr01_clicked()
+{
+
+}
+
+void MainWindow::on_salve_01_ctr02_clicked()
+{
+
+}
+
+void MainWindow::on_salve_01_ctr03_clicked()
+{
+
+}
+
+void MainWindow::on_salve_01_ctr04_clicked()
+{
+
+}
+
+void MainWindow::on_salve_01_ctr05_clicked()
+{
+
+}
+
+void MainWindow::on_salve_01_ctr06_clicked()
+{
+
+}
+
+void MainWindow::on_salve_02_ctr01_clicked()
+{
+
+}
+
+void MainWindow::on_salve_02_ctr02_clicked()
+{
+
+}
+
+void MainWindow::on_salve_02_ctr03_clicked()
+{
+
+}
+
+void MainWindow::on_salve_02_ctr04_clicked()
+{
+
+}
+
+void MainWindow::on_salve_02_ctr05_clicked()
+{
+
+}
+
+void MainWindow::on_salve_02_ctr06_clicked()
+{
+
+}
+
+void MainWindow::on_salve_03_ctr01_clicked()
+{
+
+}
+
+void MainWindow::on_salve_03_ctr02_clicked()
+{
+
+}
+
+void MainWindow::on_salve_03_ctr03_clicked()
+{
+
+}
+
+void MainWindow::on_salve_03_ctr04_clicked()
+{
+
+}
+
+void MainWindow::on_salve_03_ctr05_clicked()
+{
+
+}
+
+void MainWindow::on_salve_03_ctr06_clicked()
+{
+
+}
+
+void MainWindow::on_salve_04_ctr01_clicked()
+{
+
+}
+
+void MainWindow::on_salve_04_ctr02_clicked()
+{
+
+}
+
+void MainWindow::on_salve_04_ctr03_clicked()
+{
+
+}
+
+void MainWindow::on_salve_04_ctr04_clicked()
+{
+
+}
+
+void MainWindow::on_salve_04_ctr05_clicked()
+{
+
+}
+
+void MainWindow::on_salve_04_ctr06_clicked()
+{
+
+}
+
+void MainWindow::on_salve_05_ctr01_clicked()
+{
+
+}
+
+void MainWindow::on_salve_05_ctr02_clicked()
+{
+
+}
+
+void MainWindow::on_salve_05_ctr03_clicked()
+{
+
+}
+
+void MainWindow::on_salve_05_ctr04_clicked()
+{
+
+}
+
+void MainWindow::on_salve_05_ctr05_clicked()
+{
+
+}
+
+void MainWindow::on_salve_05_ctr06_clicked()
+{
+
 }
--- app/qt/QT_serial/mainwindow.h
+++ app/qt/QT_serial/mainwindow.h
@@ -25,6 +25,12 @@ enum DEVICES_TYPE
     DEVICES_TYPE_SALVE=1,
 };
 
+enum REV_DATA_TYPE
+{
+    DATA_IS_MASTER = 1,
+    DATA_IS_DATA = 2,
+};
+
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -41,39 +47,101 @@ public:
 
 public:
     QTimer *m_timer;
-#if USE_DEBUG
-    QSerialPort *m_serial;
-#else
-    float m_CH_value[3][6];
+    QTimer *m_master_timer;
+    QTimer *m_slave_timer;
 
-    unsigned long long int m_ID;                // 设备ID，8个字节
+    float m_CH_value[3][6];     // 保持3路通道的电流电压值
+    char m_ctl_value[5];
+
+    QString m_ID;                // 设备ID，8个字节
+    quint64 m_id;
+    QList<QString> m_dev_list;
     int m_device_type;                          // 设备类型，主机还是从机
     int m_comm_rev_flag;                        // 电力载波通信接口是否有收到数据的flag
     QSerialPort *m_serial_vol_curr_CH01;        // 采集电压、电流的第1路串口
     QSerialPort *m_serial_vol_curr_CH02;        // 采集电压、电流的第2路串口
     QSerialPort *m_serial_vol_curr_CH03;        // 采集电压、电流的第3路串口
     QSerialPort *m_serial_power_communication;  // 电力载波通信串口
-#endif
+
 
 
 public:
-#if USE_DEBUG
-#else
     int parse_uart_data();
-    void parseMeasurementData(int index_salve, QString inputs);     // 解析电压、电流的串口数据
-#endif
+    void parse_vol_cur_data(int index_salve, QString inputs);     // 解析电压、电流的串口数据
+    int parse_comm_data(QString str);
+    int check_data(const char *data, int len);
+
 private slots:
-#if USE_DEBUG
-    void read_serial_data();
-    void on_pushButton_clicked();
-#else
+
     void read_serial_vol_curr_CH01();
     void read_serial_vol_curr_CH02();
     void read_serial_vol_curr_CH03();
     void read_serial_power_communication();
-#endif
+
 
     void on_timeout();
+    void on_master_timeout();
+    void on_slave_timeout();
+
+    void on_salve_01_ctr01_clicked();
+
+    void on_salve_01_ctr02_clicked();
+
+    void on_salve_01_ctr03_clicked();
+
+    void on_salve_01_ctr04_clicked();
+
+    void on_salve_01_ctr05_clicked();
+
+    void on_salve_01_ctr06_clicked();
+
+    void on_salve_02_ctr01_clicked();
+
+    void on_salve_02_ctr02_clicked();
+
+    void on_salve_02_ctr03_clicked();
+
+    void on_salve_02_ctr04_clicked();
+
+    void on_salve_02_ctr05_clicked();
+
+    void on_salve_02_ctr06_clicked();
+
+    void on_salve_03_ctr01_clicked();
+
+    void on_salve_03_ctr02_clicked();
+
+    void on_salve_03_ctr03_clicked();
+
+    void on_salve_03_ctr04_clicked();
+
+    void on_salve_03_ctr05_clicked();
+
+    void on_salve_03_ctr06_clicked();
+
+    void on_salve_04_ctr01_clicked();
+
+    void on_salve_04_ctr02_clicked();
+
+    void on_salve_04_ctr03_clicked();
+
+    void on_salve_04_ctr04_clicked();
+
+    void on_salve_04_ctr05_clicked();
+
+    void on_salve_04_ctr06_clicked();
+
+    void on_salve_05_ctr01_clicked();
+
+    void on_salve_05_ctr02_clicked();
+
+    void on_salve_05_ctr03_clicked();
+
+    void on_salve_05_ctr04_clicked();
+
+    void on_salve_05_ctr05_clicked();
+
+    void on_salve_05_ctr06_clicked();
 
 private:
     Ui::MainWindow *ui;
